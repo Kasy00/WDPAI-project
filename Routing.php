9ef1dcec46de43ebd2c900c8c3584195079dd682
Kamil <kamilsyktu3@gmail.com>
2023-12-10 10:36:34 +0100

    Code refactor

--- Routing.php
+++ Routing.php
@@ -1,11 +1,16 @@
 <?php
 
 require_once 'src/controllers/DefaultController.php';
+require_once 'src/controllers/SecurityController.php';
 
 class Routing{
     public static $routes;
 
     public static function get($url, $view){
+        self::$routes[$url] = $view;
+    }
+
+    public static function post($url, $view){
         self::$routes[$url] = $view;
     }
 
@@ -19,7 +24,7 @@ class Routing{
         $controller = self::$routes[$action];
         $object = new $controller;
         $action = $action ?: 'index';
-        
+
         $object->$action();
     }
 }
