e54d6ea35bff868b61ad783e53f4337a1645ec29
Kamil Syktus <kamilsyktus3@gmail.com>
2023-12-14 17:00:29 +0100

    Added connect function

--- Database.php
+++ Database.php
@@ -14,4 +14,18 @@ class Database{
         $this->host = HOST;
         $this->database = DATABASE;
     }
+
+    public function connect(){
+        try{
+            $conn = new PDO(
+                "pgsql:host=$this->host;port=5432;dbname=$this->database",
+                this->username,
+                this->password,
+                ["sslmode" => "prefer"]
+            );
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        }
+    }
 }
