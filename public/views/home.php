14c25f46fcbd12f11449f7011c9b8acf8932b303
Kamil Syktus <kamilsyktus3@gmail.com>
2023-12-20 16:26:19 +0100

    Added sidebar with buttons, icons and basic styling

--- public/views/home.php
+++ public/views/home.php
@@ -9,11 +9,11 @@
 <body>
     <div class="container">
         <div class="sidebar">
-            <img src="public/img/logo.png" alt="logo">
+            <img class="logo" src="public/img/logo.png" alt="logo">
             <ul>
-                <li>Home</li>
-                <li>Profile</li>
-                <li>About</li>
+                <li><img class="sidebar-btn" src="public/img/home.svg" alt="home button">Home</li>
+                <li><img class="sidebar-btn" src="public/img/profile.svg" alt="profile button">Profile</li>
+                <li><img class="sidebar-btn" src="public/img/about.svg" alt="about button">About</li>
             </ul>
         </div>
     </div>
