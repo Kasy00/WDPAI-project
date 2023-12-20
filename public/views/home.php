33b88ab47ae00b49f850bc19055bfc2bdc3a384f
Kamil Syktus <kamilsyktus3@gmail.com>
2023-12-20 16:11:35 +0100

    Added controller and styling

--- public/views/home.php
+++ public/views/home.php
@@ -4,8 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="public/css/home-style.css">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="sidebar">
+            <img src="public/img/logo.png" alt="logo">
+            <ul>
+                <li>Home</li>
+                <li>Profile</li>
+                <li>About</li>
+            </ul>
+        </div>
+    </div>
 </body>
 </html>
