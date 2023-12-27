34e342daaa64778c56ad25dcfa94335a16fa43ab
Kamil <kamilsyktu3@gmail.com>
2023-12-27 14:50:47 +0100

    Added catch block

--- Database.php
+++ Database.php
@@ -27,5 +27,8 @@ class Database{
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
         }
+        catch(PDOException $e) {
+            die("Connection failed: " . $e->getMessage());
+        }
     }
 }
