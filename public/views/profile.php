d600170ed5bf727a1b3efdcf203937cb23983f01
Kasy00 <kamilsyktus3@gmail.com>
2023-12-31 11:54:56 +0100

    Added profile page components and basic layout.

--- public/views/profile.php
+++ public/views/profile.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
     <link rel="stylesheet" href="/public/css/home-style.css">
+    <link rel="stylesheet" href="/public/css/profile-style.css">
 </head>
 <body>
     <div class="container">
@@ -27,6 +28,18 @@
                     </nav>
                 </div>
                 <h1>MEALMAGIC</h1>
+            </div>
+        </div>
+        <div class="profile-container">
+            <div class="profile-card">
+                <img id="profile-avatar" src="/public/img/avatar1.svg" alt="avatar">
+                <h2>John Snow</h2>
+                <ul>
+                    <li><img src="/public/img/settings.svg" alt="settings">Settings</li>
+                    <li><img src="/public/img/BMI.svg" alt="BMI calculator">BMI Calculator</li>
+                    <li><img src="/public/img/favourites.svg" alt="favourites recipes">Favourites recipes</li>
+                    <li><img src="/public/img/logout.svg" alt="logout">Logout</li>
+                </ul>
             </div>
         </div>
     </div>
