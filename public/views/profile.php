1cf75e361b80794194d982955e4ced9b2301a578
Kasy00 <kamilsyktus3@gmail.com>
2023-12-31 11:14:08 +0100

    Added profile page

---
+++ public/views/profile.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="/public/css/home-style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="sidebar">
+            <img class="logo" src="/public/img/logo.png" alt="logo">
+            <ul>
+                <li><img class="sidebar-btn" src="/public/img/home.svg" alt="home button">Home</li>
+                <li><img class="sidebar-btn" src="/public/img/profile.svg" alt="profile button">Profile</li>
+                <li><img class="sidebar-btn" src="/public/img/about.svg" alt="about button">About</li>
+            </ul>
+            <div class="mobile-menu">
+                <label class="hamburger-menu">
+                    <input type="checkbox"/>
+                </label>
+                <div class="mobile-sidebar">
+                    <nav>
+                        <div>Home</div>
+                        <div>Profile</div>
+                        <div>About</div>
+                    </nav>
+                </div>
+                <h1>MEALMAGIC</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
